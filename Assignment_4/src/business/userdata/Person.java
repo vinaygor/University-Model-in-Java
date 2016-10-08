de92fe3406a9b0f03173cdfc6546349c05eaadb5
Vinay <gor.v@husky.neu.edu>
2016-10-08 00:28:16 -0400

    Uploading version 1.1 - Added Person class and subpackages

---
+++ Assignment_4/src/business/userdata/Person.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.userdata;
+
+/**
+ *
+ * @author vinay
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String personID;
+    private String gender;
+    private String dateOfBirth;
+    
+    private Address address;
+
+    public Person() {
+        this.address = new Address();
+    }
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+      
+}
